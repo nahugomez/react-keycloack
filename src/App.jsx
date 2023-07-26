1f326635a3457449e179d9595f0d04d613e729f4
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-07-26 09:53:32 -0300

    Basic Page Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { CssBaseline, Container, Box } from "@mui/material";
+import Home from "./pages/Home";
+import Welcome from "./pages/Welcome";
+import Public from "./pages/Public";
+import UserDetails from "./pages/UserDetails";
+import Manager from "./pages/Manager";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Initial Structure</h1>
-    </>
+    <BrowserRouter>
+      <CssBaseline />
+      <Box width={"100%"} sx={{ backgroundColor: "#333", color: "#FFF" }}>
+        <Container sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <p style={{ fontWeight: "bold", fontSize: "1.5rem" }}>KeyReact</p>
+          <Box>
+            <Link to={"/welcome"} style={{ marginRight: "1rem", color: "#FFF" }}>
+              Welcome
+            </Link>
+            <Link to={"/public"} style={{ marginRight: "1rem", color: "#FFF" }}>
+              Public
+            </Link>
+            <Link to={"/manager"} style={{ marginRight: "1rem", color: "#FFF" }}>
+              Managment
+            </Link>
+            <Link to={"/user-details"} style={{ marginRight: "1rem", color: "#FFF" }}>
+              User
+            </Link>
+          </Box>
+        </Container>
+      </Box>
+      <Container sx={{ marginY: "2rem" }}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/public" element={<Public />} />
+          <Route path="/manager" element={<Manager />} />
+          <Route path="/user-details" element={<UserDetails />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
