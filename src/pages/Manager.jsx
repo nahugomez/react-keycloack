d1096bf46fc16140bdb138bdad950cd383c5cd5b
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-07-26 10:41:13 -0300

    Manager Page Modified

--- src/pages/Manager.jsx
+++ src/pages/Manager.jsx
@@ -6,29 +6,9 @@ const Manager = () => {
   return (
     <div>
       {keycloak && keycloak.hasResourceRole("manager") ? (
-        <div>
-          <div>
-            <table>
-              <tr>
-                <th>Name</th>
-                <th>Second Name</th>
-                <th>Id</th>
-              </tr>
-              <tr>
-                <td>Martin</td>
-                <td>McFly</td>
-                <td>1985</td>
-              </tr>
-              <tr>
-                <td>Hemmet</td>
-                <td>Brown</td>
-                <td>1955</td>
-              </tr>
-            </table>
-          </div>
-        </div>
+        <p>Estas autenticado y tenes rol de Manager</p>
       ) : (
-        <div>Access denied. You don't have right to access this page.</div>
+        <p>No estas autenticado, o no tenes los permisos necesarios. Redirigiendo a Keycloak para realizar autenticacion</p>
       )}
     </div>
   );
