dcb0debc64aad931cc98b9241745e8bdd36a02f0
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-07-26 10:45:24 -0300

    UserDetails Auth Verification

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import useKeyCloak from "../hooks/useKeycloak";
 
 const UserDetails = () => {
-  return <div>UserDetails</div>;
+  const keycloak = useKeyCloak();
+  return (
+    <div>
+      {keycloak && keycloak.hasResourceRole("user") ? (
+        <p>Estas autenticado y tenes rol de Usuario</p>
+      ) : (
+        <p>No estas autenticado, o no tenes los permisos necesarios. Redirigiendo a Keycloak para realizar autenticacion</p>
+      )}
+    </div>
+  );
 };
 
 export default UserDetails;
